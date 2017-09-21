b8b9ba2e37e023be262c3711143d41571198996b
isaac.muñoz <isaac.muoz@CVT0698>
2017-09-21 17:23:08 +0200

    cambioController3

--- NewProjectImc/NewProjectImc/Controllers/HomeController.cs
+++ NewProjectImc/NewProjectImc/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace NewProjectImc.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page 3.";
 
             return View();
         }
